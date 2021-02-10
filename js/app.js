2450b0016c200cd25810e6fcd2bdb448968e9d60
mdmahbubulalam <alam.diu13@gmail.com>
2021-02-10 22:41:12 +0600

    added recent covid status

--- js/app.js
+++ js/app.js
@@ -1,31 +1,64 @@
 const searchCountry = country => {
     const countryInput = document.getElementById('countryInput').value;
     const countryName = countryInput.charAt(0).toUpperCase() + countryInput.slice(1);
-    const url = `https://covid-api.mmediagroup.fr/v1/cases?country=${countryName}`;
-    fetch(url)
-        .then(res => res.json())
-        .then(data => displayCovidStatus(data.All))
+    
+    const mainUrl = `https://covid-api.mmediagroup.fr/v1/cases?country=${countryName}`;
+    const statusDeathUrl = `https://covid-api.mmediagroup.fr/v1/history?country=${countryName}&status=deaths`;
+    const statusConfirmedUrl = `https://covid-api.mmediagroup.fr/v1/history?country=${countryName}&status=confirmed`;
+    const statusRecoveredUrl = `https://covid-api.mmediagroup.fr/v1/history?country=${countryName}&status=recovered `;
+
+
+    Promise.all([
+            fetch(mainUrl)
+            .then(resMain => resMain.json()),
+            fetch(statusDeathUrl)
+            .then(resDeath => resDeath.json()),
+            fetch(statusConfirmedUrl)
+            .then(resConfirmed => resConfirmed.json()),
+            fetch(statusRecoveredUrl)
+            .then(resRecovered => resRecovered.json()),
+        ])
+        .then((data) => displayCovidStatus(data[0].All, data[1].All, data[2].All, data[3].All))
         .catch(error => errorText('Please enter a valid country name'))
 }
 
-const displayCovidStatus = status => {
+const displayCovidStatus = (statusMain, statusDeath, statusConfirmed, statusRecovered) => {
+
+    const statusConfirmedObj =statusConfirmed.dates;
+    const statusConfirmedObjValue = Object.values(statusConfirmedObj);
+    const newConfirmed=statusConfirmedObjValue[0]-statusConfirmedObjValue[1];
+
+    const statusRecoveredObj =statusRecovered.dates;
+    const statusRecoveredObjValue = Object.values(statusRecoveredObj);
+    const newRecovered=statusRecoveredObjValue[0]-statusRecoveredObjValue[1];
+
+    const statusDeathObj =statusDeath.dates;
+    const statusDeathObjValue = Object.values(statusDeathObj);
+    const newDeath=statusDeathObjValue[0]-statusDeathObjValue[1];
+
     const errorMessage = document.getElementById('errorText');
     errorMessage.innerText = '';
+
     const covidDetails = document.getElementById('covidDetails');
-    const div = document.createElement('div');
-    div.className = 'covidDetails';
-    div.innerHTML = `
-        <p class="textSize">${'<b>Country</b>: '+status.country}</p>
-        <p class="textSize">${'<b>Capital</b>: '+status.capital_city}</p>
-        <p class="textSize">${'<b>Continent</b>: '+status.continent}</p>
-        <p class="textSize">${'<b>Location</b>: '+status.location}</p>
-        <p class="textSize">${'<b>Area</b>: '+status.sq_km_area+'km<sup>2</sup>'}</p>
-        <p class="textSize">${'<b>Population</b>: '+status.population}</p>
-        <p class="textSize">${'<b>Life Expectancy</b>: '+status.life_expectancy}</p>
-        <p class="textSize text-warning">${'<b>Confirmed</b>: '+status.confirmed}</p>
-        <p class="textSize text-success">${'<b>Recovered</b>: '+status.recovered}</p>
-        <p class="textSize text-danger">${'<b>Deaths</b>: '+status.deaths}</p>
-        <p class="textSize">${'<b>Elevation in Meters</b>: '+status.elevation_in_meters}</p>
+    const covidDetailsDiv = document.createElement('div');
+    covidDetailsDiv.innerHTML='';
+    covidDetailsDiv.className = 'covidDetails';
+
+    covidDetailsDiv.innerHTML = `
+        <p class="textSize">${'<b>Country</b>: '+statusMain.country}</p>
+        <p class="textSize">${'<b>Capital</b>: '+statusMain.capital_city}</p>
+        <p class="textSize">${'<b>Continent</b>: '+statusMain.continent}</p>
+        <p class="textSize">${'<b>Location</b>: '+statusMain.location}</p>
+        <p class="textSize">${'<b>Area</b>: '+statusMain.sq_km_area+'km<sup>2</sup>'}</p>
+        <p class="textSize">${'<b>Population</b>: '+statusMain.population}</p>
+        <p class="textSize">${'<b>Life Expectancy</b>: '+statusMain.life_expectancy}</p>
+        <p class="textSize text-warning">${'<b>Confirmed</b>: '+statusMain.confirmed}</p>
+        <p class="textSize text-success">${'<b>Recovered</b>: '+statusMain.recovered}</p>
+        <p class="textSize text-danger">${'<b>Deaths</b>: '+statusMain.deaths}</p>
+        <p class="textSize text-warning">${'<b>New Confirmed</b>: '+newConfirmed}</p>
+        <p class="textSize text-success">${'<b>New Recovered</b>: '+newRecovered}</p>
+        <p class="textSize text-danger">${'<b>New Deaths</b>: '+newDeath}</p>
+        <p class="textSize">${'<b>Elevation in Meters</b>: '+statusMain.elevation_in_meters}</p>
     `
     covidDetails.appendChild(div);
 }
